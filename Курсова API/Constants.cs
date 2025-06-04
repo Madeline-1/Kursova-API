d2e34d4df0fccc8f241ba7d276bef5efee030e8a
Madeline <dancent124@gmail.com>
2025-06-04 09:58:14 +0300

    Update Constants.cs

--- Курсова API/Constants.cs
+++ Курсова API/Constants.cs
@@ -3,7 +3,7 @@
     public class Constants
     {
         public static string Address = "https://spotify23.p.rapidapi.com/track_lyrics/?id=";
-        public static string ApiKey = "821ada0fcdmsh1b0d11162b8aeb6p1e23a7jsnb2a981386ca7";
+        public static string ApiKey = "МійКлюч";
         public static string ApiHost = "spotify23.p.rapidapi.com";
     }
 }
